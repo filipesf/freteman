3a4d5dbd6fb90261bf863123724606ca5075fd32
Filipe Fernandes <me@filipesf.com>
2017-11-24 16:01:51 +0000

    Added Nav

---
+++ source/components/Nav/SiteNav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './SiteNav.scss';
+
+import Logo from '../Logo/Logo';
+import '../Button/Button';
+
+const SiteNav = props => {
+  return (
+    <nav className="site-nav">
+      <Logo />
+      <ul className="site-nav__list">
+        <li className="site-nav__item"><a href="#Frota" className="site-nav__link">Frota</a></li>
+        <li className="site-nav__item"><a href="#Serviços" className="site-nav__link">Serviços</a></li>
+        <li className="site-nav__item"><a href="#Contato" className="site-nav__link">Contato</a></li>
+        <li className="site-nav__item"><a href="#Login" className="site-nav__link">Login</a></li>
+        <li className="site-nav__item"><a href="#Button" className="btn btn--outline btn--accent">Texto Marcação</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default SiteNav;
