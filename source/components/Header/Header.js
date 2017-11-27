d7d73fe4d1f34679f7352da0e07af8c8ee9d36a5
Filipe Fernandes <me@filipesf.com>
2017-11-27 17:12:09 +0000

    Imported headline component

--- source/components/Header/Header.js
+++ source/components/Header/Header.js
@@ -3,12 +3,18 @@ import bg from './header-bg.svg';
 import './Header.scss';
 
 import SiteNav from '../Nav/SiteNav';
+import Headline from '../Headline/Headline';
 
 const Header = props => {
   return (
     <header className="site-header" style={{backgroundImage: 'url('+ bg +')'}}>
       <div className="l-wrapper">
         <SiteNav />
+        <Headline
+          type="header"
+          title="Entregamos qualquer coisa em qualquer lugar"
+          subtitle="Atendemos entregas de mercadorias, cargas, móveis e mudanças."
+        />
       </div>
     </header>
   );
