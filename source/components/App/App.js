b6ad953da82143f2ba9ecae79b6fbec809b73f04
Filipe Fernandes <me@filipesf.com>
2017-11-27 17:11:02 +0000

    Added footer

--- source/components/App/App.js
+++ source/components/App/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.scss';
 
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 class App extends Component {
   render() {
     return (
       <div id="FretemanApp">
         <Header />
+        <Footer />
       </div>
     );
   }
---
+++ source/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../styles/_helpers.scss';
+
+const Footer = props => {
+  return <footer></footer>;
+}
+
+export default Footer;
