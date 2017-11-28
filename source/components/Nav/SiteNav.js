1920a8209d5164047084896a19436f83581c07c6
Filipe Fernandes <me@filipesf.com>
2017-11-28 16:56:48 +0000

    Added new site-nav__container

--- source/components/Nav/SiteNav.js
+++ source/components/Nav/SiteNav.js
@@ -7,14 +7,16 @@ import '../Button/Button';
 const SiteNav = props => {
   return (
     <nav className="site-nav">
-      <Logo />
-      <ul className="site-nav__list">
-        <li className="site-nav__item"><a href="#Frota" className="site-nav__link">Frota</a></li>
-        <li className="site-nav__item"><a href="#Serviços" className="site-nav__link">Serviços</a></li>
-        <li className="site-nav__item"><a href="#Contato" className="site-nav__link">Contato</a></li>
-        <li className="site-nav__item"><a href="#Login" className="site-nav__link">Login</a></li>
-        <li className="site-nav__item"><a href="#Button" className="btn btn--outline btn--accent">Texto Marcação</a></li>
-      </ul>
+      <div className="site-nav__container">
+        <Logo />
+        <ul className="site-nav__list">
+          <li className="site-nav__item"><a href="#Frota" className="site-nav__link">Frota</a></li>
+          <li className="site-nav__item"><a href="#Serviços" className="site-nav__link">Serviços</a></li>
+          <li className="site-nav__item"><a href="#Contato" className="site-nav__link">Contato</a></li>
+          <li className="site-nav__item"><a href="#Login" className="site-nav__link">Login</a></li>
+          <li className="site-nav__item"><a href="#Button" className="btn btn--outline btn--accent">Texto Marcação</a></li>
+        </ul>
+      </div>
     </nav>
   );
 }
