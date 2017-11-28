e5583ead2a387ddae6b6908b1bc06c800f3e198d
Filipe Fernandes <me@filipesf.com>
2017-11-28 16:56:48 +0000

    Added Block component

---
+++ source/components/Block/Block.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import './Block.scss';
+
+const Block = ({shadow, radius, ...props}) => {
+  const classes = classNames(
+    'c-block',
+    shadow && `u-box-shadow`,
+    radius && `u-border-radius`
+  );
+
+  return (
+    <div {...props} className={classes}>
+      {props.children}
+    </div>
+  );
+}
+
+Block.PropTypes = {
+  children: PropTypes.element.isRequired,
+  shadow: PropTypes.bool,
+  radius: PropTypes.bool
+}
+
+export default Block;
