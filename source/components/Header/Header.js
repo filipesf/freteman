36cc38b9864399fcc3f30393d0ccba37de41d03e
Filipe Fernandes <me@filipesf.com>
2017-11-28 16:56:48 +0000

    Removed SiteNav; Added Block component; Header Restructured

--- source/components/Header/Header.js
+++ source/components/Header/Header.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import bg from './header-bg.svg';
+import trucks from './trucks.png';
 import './Header.scss';
 
-import SiteNav from '../Nav/SiteNav';
 import Headline from '../Headline/Headline';
+import Block from '../Block/Block';
 
 const Header = props => {
   return (
     <header className="site-header" style={{backgroundImage: 'url('+ bg +')'}}>
-      <div className="l-wrapper">
-        <SiteNav />
-        <Headline
-          type="header"
-          title="Entregamos qualquer coisa em qualquer lugar"
-          subtitle="Atendemos entregas de mercadorias, cargas, móveis e mudanças."
-        />
+      <div className="site-header__container">
+        <section className="site-header__section">
+          <Headline
+            title="Entregamos qualquer coisa em qualquer lugar"
+            subtitle="Atendemos entregas de mercadorias, cargas, móveis e mudanças."
+            modifier="header"
+          />
+          <figure className="site-header__image">
+            <img src={trucks} alt="Trucks" />
+          </figure>
+        </section>
+        <section className="site-header__section">
+          <Block shadow={true} radius={true}></Block>
+        </section>
       </div>
     </header>
   );
