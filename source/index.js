0c98589f7fd8728c142e614306951652db7d684e
Filipe Fernandes <me@filipesf.com>
2017-11-29 15:41:22 +0000

    Updated Applictaion import

--- source/index.js
+++ source/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/Application/App';
+import App from './containers/Application';
 import registerServiceWorker from './sw';
 
 ReactDOM.render(<App />, document.getElementById('root'));
