cd282f640fe7c6ebbc3d6f8e56c2acd265d64aa4
Filipe Fernandes <me@filipesf.com>
2017-11-30 11:51:18 +0000

    Moved main syles to index.js

--- source/index.js
+++ source/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/Application';
+import App from './pages/LandingPage';
+import './assets/styles/main.scss';
 import registerServiceWorker from './sw';
 
 ReactDOM.render(<App />, document.getElementById('root'));
