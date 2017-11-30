f095484d4521b13e1faa7b8e3f312ce56b0d77ee
Filipe Fernandes <me@filipesf.com>
2017-11-30 17:02:44 +0000

    Added form and form styles

--- source/components/Header/index.js
+++ source/components/Header/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import bg from './assets/header-bg.svg';
 import trucks from './assets/trucks.png';
+
+import arrowCollection from '../../assets/images/arrow-filled.svg';
+import arrowDelivery from '../../assets/images/arrow-outline.svg';
+
 import './assets/header.scss';
+import './assets/forms.scss';
 
 import Headline from '../Headline';
 import Block from '../../containers/Block';
@@ -21,7 +26,39 @@ const Header = props => {
           </figure>
         </section>
         <section className="section">
-          <Block shadow={true} radius={true}></Block>
+          <Block shadow={true} radius={true}>
+            <form>
+              <fieldset className="c-fieldset -collection" style={{backgroundImage: 'url('+ arrowCollection +')'}}>
+                <label htmlFor="collection-address">Endereço de coleta</label>
+                <input id="collection-address" type="text" placeholder="Digite um endereço" />
+              </fieldset>
+
+              <fieldset className="c-fieldset -delivery" style={{backgroundImage: 'url('+ arrowDelivery +')'}}>
+                <label htmlFor="delivery-address">Endereço de entrega</label>
+                <input id="delivery-address" type="text" placeholder="Digite um endereço" />
+              </fieldset>
+
+              <fieldset className="c-fieldset">
+                <input type="radio" name="veicle" value="Fiorino" />
+                <input type="radio" name="veicle" value="Van" />
+                <input type="radio" name="veicle" value="Caminhão" />
+              </fieldset>
+
+              <section>
+                <div>
+                  <h5>Distância</h5>
+                  <p>54km e 200m</p>
+                </div>
+
+                <div>
+                  <h5>Valor</h5>
+                  <p>R$ 1264,00</p>
+                </div>
+              </section>
+
+              <button className="c-btn -block -large -accent">Chamar Mensageiro</button>
+            </form>
+          </Block>
         </section>
       </div>
     </header>
