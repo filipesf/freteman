d2e7141ce73d48f539460737513119630e8e2f6f
Filipe Fernandes <me@filipesf.com>
2017-11-30 12:06:59 +0000

    Updated CTA copy

--- source/components/SiteNav/index.js
+++ source/components/SiteNav/index.js
@@ -14,7 +14,7 @@ const SiteNav = props => {
           <li className="item"><a href="#Serviços" className="link">Serviços</a></li>
           <li className="item"><a href="#Contato" className="link">Contato</a></li>
           <li className="item"><a href="#Login" className="link">Login</a></li>
-          <li className="item"><a href="#Button" className="c-btn -outline -accent">Texto Marcação</a></li>
+          <li className="item"><a href="#Button" className="c-btn -outline -accent">Fazer Orçamento</a></li>
         </ul>
       </div>
     </nav>
