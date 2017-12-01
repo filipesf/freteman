61828b314d00b2524ad24b47d61fc50f6225d9d5
Filipe Fernandes <me@filipesf.com>
2017-12-01 17:14:39 +0000

    Added react-svg-inline

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -131,12 +131,16 @@ module.exports = {
           // smaller than specified limit in bytes as data URLs to avoid requests.
           // A missing `test` is equivalent to a match.
           {
-            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
             loader: require.resolve('url-loader'),
             options: {
               limit: 10000,
               name: 'static/media/[name].[hash:8].[ext]',
             },
+          },
+          {
+            test: /\.svg$/,
+            loader: 'raw-loader'
           },
           // Process JS with Babel.
           {
