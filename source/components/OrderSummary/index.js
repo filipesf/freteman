a279800510eb5b35b22d3385d116cb34720fcf6f
Filipe Fernandes <me@filipesf.com>
2017-12-03 21:51:45 +0000

    Added removed spacing and reordered items

--- source/components/OrderSummary/index.js
+++ source/components/OrderSummary/index.js
@@ -3,14 +3,14 @@ import './assets/order-summary.scss';
 
 const OrderSummary = props => {
   return (
-    <ul className="c-order-summary">
+    <ul className="c-order-summary _flat-grid">
       <li className="item">
         <span className="name">Distância</span>
-        <span className="value">54km e 200m</span>
+        <span className="name">Valor Total</span>
       </li>
 
       <li className="item">
-        <span className="name -large">Valor Total</span>
+        <span className="value">54km e 200m</span>
         <span className="value -large">R$ 1264,00</span>
       </li>
     </ul>
