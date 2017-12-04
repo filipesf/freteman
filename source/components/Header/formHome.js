1c67f288b52535a11cf7022375270ba513deba03
Filipe Fernandes <me@filipesf.com>
2017-12-04 14:47:41 +0000

    Updated CSS refactored classes

--- source/components/Header/formHome.js
+++ source/components/Header/formHome.js
@@ -15,45 +15,45 @@ const FormHome = props => {
     <form>
       <section className="c-fieldset has-icon">
         <SVG className="icon" classSuffix="" svg={arrowCollection} />
-        <div className="field">
+        <div className="c-field">
           <label className="label" htmlFor="collection-address">Endereço de coleta</label>
-          <input className="input _flat" id="collection-address" type="text" placeholder="Digite um endereço" />
+          <input className="c-input -text _nospacing _noborder _noshadow" id="collection-address" type="text" placeholder="Digite um endereço" />
         </div>
       </section>
 
       <section className="c-fieldset has-icon">
         <SVG className="icon" classSuffix="" svg={arrowDelivery} />
-        <div className="field">
+        <div className="c-field">
           <label className="label" htmlFor="delivery-address">Endereço de entrega</label>
-          <input className="input _flat" id="delivery-address" type="text" placeholder="Digite um endereço" />
+          <input className="c-input -text _nospacing _noborder _noshadow" id="delivery-address" type="text" placeholder="Digite um endereço" />
         </div>
       </section>
 
       <hr />
 
       <section className="c-fieldset has-veicles">
-        <div className="field -radio">
-          <label htmlFor="Fiorino" className="c-btn -radio -icon">
+        <div className="c-field">
+          <input id="Fiorino" type="radio" name="veicle" value="Fiorino" />
+          <label className="c-input -radio has-icon" htmlFor="Fiorino">
             <SVG className="icon" classSuffix="" svg={carFiorino} />
             <span className="text">Fiorino</span>
           </label>
-          <input id="Fiorino" type="radio" name="veicle" value="Fiorino" />
         </div>
 
-        <div className="field -radio">
-          <label htmlFor="Van" className="c-btn -radio -icon">
+        <div className="c-field">
+          <input id="Van" type="radio" name="veicle" value="Van" />
+          <label className="c-input -radio has-icon" htmlFor="Van">
             <SVG className="icon" classSuffix="" svg={carUtil} />
             <span className="text">Van</span>
           </label>
-          <input id="Van" type="radio" name="veicle" value="Van" />
         </div>
 
-        <div className="field -radio">
-          <label htmlFor="Caminhão" className="c-btn -radio -icon">
+        <div className="c-field">
+          <input id="Caminhão" type="radio" name="veicle" value="Caminhão" />
+          <label className="c-input -radio has-icon" htmlFor="Caminhão">
             <SVG className="icon" classSuffix="" svg={carTruck} />
             <span className="text">Caminhão</span>
           </label>
-          <input id="Caminhão" type="radio" name="veicle" value="Caminhão" />
         </div>
         <small className="detail">150kg max. P 133 x L 133 x A 133 cm</small>
       </section>
