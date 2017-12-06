c093e2b87abf63284c0eededcf55101bb4b4f432
Filipe Fernandes <me@filipesf.com>
2017-12-06 16:23:26 +0000

    Added mobile call to action

--- source/components/SiteNav/index.js
+++ source/components/SiteNav/index.js
@@ -11,13 +11,13 @@ const SiteNav = props => {
     <nav className="site-nav">
       <div className="container">
         <Logo />
+        <a href="#Button" className="c-btn -accent _show-mobile-only">Fazer Orçamento</a>
         <SVG className="i-nav" id="js-nav" svg={icon} />
         <ul className="list">
           <li className="item"><a href="#Frota" className="link">Frota</a></li>
           <li className="item"><a href="#Serviços" className="link">Serviços</a></li>
           <li className="item"><a href="#Contato" className="link">Contato</a></li>
           <li className="item"><a href="#Login" className="link">Login</a></li>
-          <li className="item"><a href="#Button" className="c-btn -outline -accent">Fazer Orçamento</a></li>
         </ul>
       </div>
     </nav>
