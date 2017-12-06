df051a28a0fbaebc9b953ccced9e277a8a1c9f1f
Filipe Fernandes <me@filipesf.com>
2017-12-06 22:53:57 +0000

    Added active class handler; moved to class component

--- source/components/SiteNav/index.js
+++ source/components/SiteNav/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import SVG from 'react-svg-inline';
 import icon from './assets/menu.svg';
 import './assets/sitenav.scss';
@@ -6,22 +6,37 @@ import './assets/sitenav.scss';
 import Logo from '../Logo';
 import '../Button';
 
-const SiteNav = props => {
-  return (
-    <nav className="site-nav">
-      <div className="container">
-        <Logo />
-        <a href="#Button" className="c-btn -accent _show-mobile-only">Fazer Orçamento</a>
-        <SVG className="i-nav" id="js-nav" svg={icon} />
-        <ul className="list">
-          <li className="item"><a href="#Frota" className="link">Frota</a></li>
-          <li className="item"><a href="#Serviços" className="link">Serviços</a></li>
-          <li className="item"><a href="#Contato" className="link">Contato</a></li>
-          <li className="item"><a href="#Login" className="link">Login</a></li>
-        </ul>
-      </div>
-    </nav>
-  );
+class SiteNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { active: false };
+    this.handleClass = this.handleClass.bind(this);
+  }
+
+  handleClass() {
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+  }
+
+  render() {
+    let activeState = this.state.active ? ' is-active' : '';
+
+    return (
+      <nav className="site-nav">
+        <div className="container">
+          <Logo />
+          <a href="#Button" className="c-btn -accent _show-mobile-only">Fazer Orçamento</a>
+          <SVG className={'i-nav' + activeState} svg={icon} onClick={this.handleClass} />
+          <ul className={'list' + activeState}>
+            <li className="item"><a href="#Frota" className="link">Frota</a></li>
+            <li className="item"><a href="#Serviços" className="link">Serviços</a></li>
+            <li className="item"><a href="#Contato" className="link">Contato</a></li>
+            <li className="item"><a href="#Login" className="link">Login</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default SiteNav;
