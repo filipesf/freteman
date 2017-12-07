f7c6046328ec99e97a62cf41a598c0808e9ec72b
Filipe Fernandes <me@filipesf.com>
2017-12-07 16:40:32 +0000

    Added images, initial sytles andmarkup

--- source/pages/LandingPage/Sections/Services.js
+++ source/pages/LandingPage/Sections/Services.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import SVG from 'react-svg-inline';
 
 import Section from '../../../containers/Section';
 import Headline from '../../../components/Headline';
+
+import '../../../assets/styles/_services.scss';
+
+import distrib  from '../../../assets/images/distribution.svg';
+import house    from '../../../assets/images/house.svg';
+import pieces   from '../../../assets/images/pieces.svg';
+import products from '../../../assets/images/products.svg';
+import trash    from '../../../assets/images/trash.svg';
 
 const Services = () =>
   <Section>
@@ -9,6 +18,28 @@ const Services = () =>
       title="Tipos de serviços"
       subtitle="Atuamos em diversas áreas para oferecer a você serviços para qualquer necessidade."
     />
+
+    <section className="l-box-container">
+      <div className="c-box _shadow-large">
+        <SVG svg={products} />
+      </div>
+
+      <div className="c-box _shadow-large">
+        <SVG svg={distrib} />
+      </div>
+
+      <div className="c-box _shadow-large">
+        <SVG svg={house} />
+      </div>
+
+      <div className="c-box _shadow-large">
+        <SVG svg={pieces} />
+      </div>
+
+      <div className="c-box _shadow-large">
+        <SVG svg={trash} />
+      </div>
+    </section>
   </Section>
 
 export default Services;
