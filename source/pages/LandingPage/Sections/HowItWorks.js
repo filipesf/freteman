5e956125f90a103b8cea97a18064eeaafa7ebc2c
Filipe Fernandes <me@filipesf.com>
2017-12-15 16:32:55 +0000

    Moved Showcase to its own component

--- source/pages/LandingPage/Sections/HowItWorks.js
+++ source/pages/LandingPage/Sections/HowItWorks.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import SVG from 'react-svg-inline';
 
 /* CONTAINERS
    ========================================================================== */
@@ -10,18 +9,44 @@ import Section   from '../../../containers/Section';
 /* COMPONENTS
    ========================================================================== */
 import Headline from '../../../components/Headline';
+import Showcase from '../../../components/Showcase';
+
+
 
 
 
 /* ASSETS
    ========================================================================== */
-import order    from '../../../assets/images/howitworks-1.png';
-import employee from '../../../assets/images/howitworks-2.png';
-import destiny  from '../../../assets/images/howitworks-3.png';
-import box  from '../../../assets/images/icons/box.svg';
-import brand  from '../../../assets/images/icons/brand.svg';
-import pin  from '../../../assets/images/icons/pin.svg';
-import './howitworks.scss';
+import iconBox       from '../../../assets/images/icons/box.svg';
+import iconBrand     from '../../../assets/images/icons/brand.svg';
+import iconPin       from '../../../assets/images/icons/pin.svg';
+import thumbOrder    from '../../../assets/images/howitworks-1.png';
+import thumbEmployee from '../../../assets/images/howitworks-2.png';
+import thumbDestiny  from '../../../assets/images/howitworks-3.png';
+
+
+
+/* DATA
+   ========================================================================== */
+const showcaseItems = [{
+  "active": true,
+  "icon": iconBox,
+  "thumb": thumbOrder,
+  "title": "Faça um pedido pelo sistema da Freteman",
+  "description": "Escolha o local de coleta e de entrega e forneça detalhes do serviço."
+},{
+  "active": false,
+  "icon": iconBrand,
+  "thumb": thumbEmployee,
+  "title": "Encontre-se com o funcionário da Freteman",
+  "description": "A encomenda é coletada por nosso funcionário e encaminhada até seu destino."
+},{
+  "active": false,
+  "icon": iconPin,
+  "thumb": thumbDestiny,
+  "title": "Seu material é entregue ao destino escolhido",
+  "description": "Sua encomenda chega nas mãos do destinário e você pode acompanhar tudo pelo sistema."
+}];
 
 
 
@@ -37,47 +62,7 @@ const HowItWorks = () =>
       subtitle="Chame um mensageiro em três passos simples"
     />
 
-    <div className="c-showcase">
-      <input className="controller" type="radio" id="howitworks-1" name="showcase" defaultChecked />
-      <input className="controller" type="radio" id="howitworks-2" name="showcase" />
-      <input className="controller" type="radio" id="howitworks-3" name="showcase" />
-
-      <ul className="details">
-        <li className="item">
-          <SVG className="icon" svg={box} />
-          <label htmlFor="howitworks-1">
-            <h4 className="title">Faça um pedido pelo sistema da Freteman</h4>
-            <p className="description">Escolha o local de coleta e de entrega e forneça detalhes do serviço.</p>
-          </label>
-        </li>
-        <li className="item">
-          <SVG className="icon" svg={brand} />
-          <label htmlFor="howitworks-2">
-            <h4 className="title">Encontre-se com o funcionário da Freteman</h4>
-            <p className="description">A encomenda é coletada por nosso funcionário e encaminhada até seu destino.</p>
-          </label>
-        </li>
-        <li className="item">
-          <SVG className="icon" svg={pin} />
-          <label htmlFor="howitworks-3">
-            <h4 className="title">Seu material é entregue ao destino escolhido</h4>
-            <p className="description">Sua encomenda chega nas mãos do destinário e você pode acompanhar tudo pelo sistema.</p>
-          </label>
-        </li>
-      </ul>
-
-      <ul className="content">
-        <li className="item"><img src={order} alt="" /></li>
-        <li className="item"><img src={employee} alt="" /></li>
-        <li className="item"><img src={destiny} alt="" /></li>
-      </ul>
-
-       <nav className="c-bullet-list">
-         <label className="bullet" htmlFor="howitworks-1"></label>
-         <label className="bullet" htmlFor="howitworks-2"></label>
-         <label className="bullet" htmlFor="howitworks-3"></label>
-       </nav>
-    </div>
+    <Showcase data={showcaseItems} />
   </Section>
 
 export default HowItWorks;
