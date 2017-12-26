e1767bfe65bb7b7bb9f52134139d985dce9c8901
Filipe Fernandes <me@filipesf.com>
2017-12-26 18:11:00 +0000

    Added assets and data

--- source/pages/LandingPage/Sections/Brands.js
+++ source/pages/LandingPage/Sections/Brands.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 import Section from '../../../containers/Section';
+import CardsList from '../../../containers/CardsList';
+
 import Headline from '../../../components/Headline';
+
+import brandLatam from '../../../assets/images/brands/latam.png';
+import brandPetrobras from '../../../assets/images/brands/petrobras.png';
+import brandVivo from '../../../assets/images/brands/vivo.png';
+import brandReclameAqui from '../../../assets/images/brands/reclameaqui.png';
+import brandConsul from '../../../assets/images/brands/consul.png';
+
+const brandCards = [
+  { image: brandLatam },
+  { image: brandPetrobras },
+  { image: brandVivo },
+  { image: brandReclameAqui },
+  { image: brandConsul }
+];
 
 const Brands = () => (
   <Section theme="tint">
@@ -9,6 +25,8 @@ const Brands = () => (
       title="Marcas"
       subtitle="Atuamos em diversas áreas para oferecer a você serviços para qualquer necessidade."
     />
+
+    <CardsList hasBrands data={brandCards} />
   </Section>
 );
 
