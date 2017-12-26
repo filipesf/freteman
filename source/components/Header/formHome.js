fdb09098741272bc2bab7dd4a899180d1afd783b
Filipe Fernandes <me@filipesf.com>
2017-12-26 13:05:15 +0000

    Moved form to BEM naming convention

--- source/components/Header/formHome.js
+++ source/components/Header/formHome.js
@@ -13,46 +13,46 @@ import './assets/forms.scss';
 const FormHome = () => (
   <form>
     <section className="l-fieldset has-icon">
-      <SvgInline className="icon" classSuffix="" svg={arrowUp} />
+      <SvgInline className="l-fieldset__icon" classSuffix="" svg={arrowUp} />
       <div className="c-field">
-        <label className="label" htmlFor="collection-address">Endereço de coleta</label>
-        <input className="c-input -text" id="collection-address" type="text" placeholder="Digite um endereço" />
+        <label className="c-field__label" htmlFor="collection-address">Endereço de coleta</label>
+        <input className="c-input --text" id="collection-address" type="text" placeholder="Digite um endereço" />
       </div>
     </section>
 
     <section className="l-fieldset has-icon">
-      <SvgInline className="icon" classSuffix="" svg={arrowDown} />
+      <SvgInline className="l-fieldset__icon" classSuffix="" svg={arrowDown} />
       <div className="c-field">
-        <label className="label" htmlFor="delivery-address">Endereço de entrega</label>
-        <input className="c-input -text" id="delivery-address" type="text" placeholder="Digite um endereço" />
+        <label className="c-field__label" htmlFor="delivery-address">Endereço de entrega</label>
+        <input className="c-input --text" id="delivery-address" type="text" placeholder="Digite um endereço" />
       </div>
     </section>
 
     <section className="l-fieldset has-veicles">
       <div className="c-field">
         <input id="Fiorino" type="radio" name="veicle" value="Fiorino" />
-        <label className="c-input -radio has-icon" htmlFor="Fiorino">
-          <SvgInline className="icon" classSuffix="" svg={iconFiorino} />
-          <span className="text">Fiorino</span>
+        <label className="c-input --radio has-icon" htmlFor="Fiorino">
+          <SvgInline className="c-input__icon" classSuffix="" svg={iconFiorino} />
+          <span className="c-input__info">Fiorino</span>
         </label>
       </div>
 
       <div className="c-field">
         <input id="Van" type="radio" name="veicle" value="Van" />
-        <label className="c-input -radio has-icon" htmlFor="Van">
-          <SvgInline className="icon" classSuffix="" svg={iconUtil} />
-          <span className="text">Van</span>
+        <label className="c-input --radio has-icon" htmlFor="Van">
+          <SvgInline className="c-input__icon" classSuffix="" svg={iconUtil} />
+          <span className="c-input__info">Van</span>
         </label>
       </div>
 
       <div className="c-field">
         <input id="Caminhão" type="radio" name="veicle" value="Caminhão" />
-        <label className="c-input -radio has-icon" htmlFor="Caminhão">
-          <SvgInline className="icon" classSuffix="" svg={iconTruck} />
-          <span className="text">Caminhão</span>
+        <label className="c-input --radio has-icon" htmlFor="Caminhão">
+          <SvgInline className="c-input__icon" classSuffix="" svg={iconTruck} />
+          <span className="c-input__info">Caminhão</span>
         </label>
       </div>
-      <small className="detail">150kg max. P 133 x L 133 x A 133 cm</small>
+      <small className="l-fieldset__detail">150kg max. P 133 x L 133 x A 133 cm</small>
     </section>
 
     <OrderSummary />
