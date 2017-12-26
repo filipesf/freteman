85d5957a662daca1abbb689f5d923b55524406e0
Filipe Fernandes <me@filipesf.com>
2017-12-26 12:13:34 +0000

    Moved order-summary to BEM naming convention

--- source/components/OrderSummary/index.js
+++ source/components/OrderSummary/index.js
@@ -5,13 +5,13 @@ import './assets/order-summary.scss';
 const OrderSummary = () => (
   <ul className="l-order-summary">
     <li className="c-order-item">
-      <span className="name">Distância</span>
-      <span className="value">54km e 200m</span>
+      <span className="c-order-item__name">Distância</span>
+      <span className="c-order-item__value">54km e 200m</span>
     </li>
 
     <li className="c-order-item">
-      <span className="name">Valor Total</span>
-      <span className="value -large">R$ 1264,00</span>
+      <span className="c-order-item__name">Valor Total</span>
+      <span className="c-order-item__value --large">R$ 1264,00</span>
     </li>
   </ul>
 );
