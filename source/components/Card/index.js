0322dec5673cb348aef83de307df957656b43141
Filipe Fernandes <me@filipesf.com>
2017-12-26 18:11:00 +0000

    Added modifier classes handler; <img> tag support; Headline checker

--- source/components/Card/index.js
+++ source/components/Card/index.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import SvgInline from 'react-svg-inline';
+import isSvg from 'is-svg';
+import cx from 'classnames';
 
 import Headline from '../Headline';
 
 import './assets/card.scss';
 
-const Card = props => (
-  <li className="c-card">
-    <figure className="c-card__image"><SvgInline svg={props.card.image} /></figure>
-    <Headline title={props.card.title} subtitle={props.card.subtitle} small />
-  </li>
-);
+const Card = props => {
+  const classes = cx(
+    'c-card',
+    props.flat && `--flat`
+  );
+
+  return (
+    <li className={classes}>
+      <figure className="c-card__image">{
+        isSvg(props.card.image)
+          ? <SvgInline svg={props.card.image} />
+          : <img src={props.card.image} />
+      }</figure>
+      {props.card.title && <Headline title={props.card.title} subtitle={props.card.subtitle} small />}
+    </li>
+  );
+};
 
 Card.propTypes = {
   card: propTypes.object,
   image: propTypes.string,
   title: propTypes.string,
   subtitle: propTypes.string,
+  flat: propTypes.bool,
 };
 
 export default Card;
