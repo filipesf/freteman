e48c6cba55e3884b03364a08398af095bc1e7ee0
Filipe Fernandes <me@filipesf.com>
2017-12-29 23:15:37 +0000

    db8f76c Importing assets from constant file

--- source/components/Header/formHome.js
+++ source/components/Header/formHome.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import SvgInline from "react-svg-inline";
-
+import assets from '../../constants/assets';
 import OrderSummary from '../OrderSummary';
-
-import arrowUp from '../../assets/images/icons/arrow-filled.svg';
-import arrowDown from '../../assets/images/icons/arrow-outline.svg';
-import iconFiorino from '../../assets/images/icons/car-fiorino.svg';
-import iconTruck from '../../assets/images/icons/car-truck.svg';
-import iconUtil from '../../assets/images/icons/car-util.svg';
 import './assets/forms.scss';
 
 const FormHome = () => (
   <form>
     <section className="l-fieldset has-icon">
-      <SvgInline className="l-fieldset__icon" svg={arrowUp} />
+      <SvgInline className="l-fieldset__icon" svg={assets.icon.arrowFilled} />
       <div className="c-field">
         <label className="c-field__label" htmlFor="collection-address">Endereço de coleta</label>
         <input className="c-input --text" id="collection-address" type="text" placeholder="Digite um endereço" />
@@ -21,7 +15,7 @@ const FormHome = () => (
     </section>
 
     <section className="l-fieldset has-icon">
-      <SvgInline className="l-fieldset__icon" svg={arrowDown} />
+      <SvgInline className="l-fieldset__icon" svg={assets.icon.arrowOutline} />
       <div className="c-field">
         <label className="c-field__label" htmlFor="delivery-address">Endereço de entrega</label>
         <input className="c-input --text" id="delivery-address" type="text" placeholder="Digite um endereço" />
@@ -32,7 +26,7 @@ const FormHome = () => (
       <div className="c-field">
         <input id="Fiorino" type="radio" name="veicle" value="Fiorino" />
         <label className="c-input --radio has-icon" htmlFor="Fiorino">
-          <SvgInline className="c-input__icon" svg={iconFiorino} />
+          <SvgInline className="c-input__icon" svg={assets.icon.fiorino} />
           <span className="c-input__info">Fiorino</span>
         </label>
       </div>
@@ -40,7 +34,7 @@ const FormHome = () => (
       <div className="c-field">
         <input id="Van" type="radio" name="veicle" value="Van" />
         <label className="c-input --radio has-icon" htmlFor="Van">
-          <SvgInline className="c-input__icon" svg={iconUtil} />
+          <SvgInline className="c-input__icon" svg={assets.icon.util} />
           <span className="c-input__info">Van</span>
         </label>
       </div>
@@ -48,7 +42,7 @@ const FormHome = () => (
       <div className="c-field">
         <input id="Caminhão" type="radio" name="veicle" value="Caminhão" />
         <label className="c-input --radio has-icon" htmlFor="Caminhão">
-          <SvgInline className="c-input__icon" svg={iconTruck} />
+          <SvgInline className="c-input__icon" svg={assets.icon.truck} />
           <span className="c-input__info">Caminhão</span>
         </label>
       </div>
