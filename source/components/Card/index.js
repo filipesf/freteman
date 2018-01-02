55aa49382a2fc66c58ec4efe407ca483f2a936d3
Filipe Fernandes <me@filipesf.com>
2018-01-02 00:13:28 +0000

    Simplified; Moved card image to Thumbnail component;

--- source/components/Card/index.js
+++ source/components/Card/index.js
@@ -1,36 +1,18 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import SvgInline from 'react-svg-inline';
-import cx from 'classnames';
-
+import Thumbnail from '../Thumbnail';
 import Headline from '../Headline';
-
 import './assets/card.scss';
 
-const Card = props => {
-  const classes = cx(
-    'c-card',
-    props.flat && `--flat`
-  );
-
-  return (
-    <li className={classes}>
-      <figure className="c-card__image">{
-        typeof props.card.image === 'string'
-          ? <img src={props.card.image} />
-          : <SvgInline svg={props.card.image} />
-      }</figure>
-      {props.card.title && <Headline title={props.card.title} subtitle={props.card.subtitle} small />}
-    </li>
-  );
-};
+const Card = props => (
+  <li className="c-card">
+    {props.content.image && <Thumbnail src={props.content.image} alt={props.content.title} />}
+    {props.content.title && <Headline title={props.content.title} subtitle={props.content.subtitle} small />}
+  </li>
+);
 
 Card.propTypes = {
-  card: propTypes.object,
-  image: propTypes.string,
-  title: propTypes.string,
-  subtitle: propTypes.string,
-  flat: propTypes.bool,
+  content: propTypes.object
 };
 
 export default Card;
