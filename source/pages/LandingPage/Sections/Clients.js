295a6b2b4786087a18bee5194e6a45249f79b957
Filipe Fernandes <me@filipesf.com>
2018-01-02 00:13:28 +0000

    Added TestimonialsList component

--- source/pages/LandingPage/Sections/Clients.js
+++ source/pages/LandingPage/Sections/Clients.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import data from '../../../constants/data';
 import Section from '../../../containers/Section';
-import Block from '../../../containers/Block';
 import Headline from '../../../components/Headline';
-import Author from '../../../components/Author';
-
-const testimonialItem = data.clients.content.map((t, index) => (
-  <Block shadow key={index}>
-    <blockquote>{t.quote}</blockquote>
-    <Author avatar={t.avatar} name={t.name} location={t.location} />
-  </Block>
-));
+import TestimonialsList from '../../../containers/TestimonialsList';
 
 const Clients = () => (
   <Section theme="red">
     <Headline title={data.clients.title} />
-    <section className="l-testimonials">{testimonialItem}</section>
+    <TestimonialsList data={data.clients.content} />
   </Section>
 );
 
