8ac5a23ac0902d1fc316a440c8ce706de7cc0a3c
Filipe Fernandes <me@filipesf.com>
2018-01-09 23:22:31 +0000

    Added Thumbnail component and social link styles

--- source/components/SocialLinks/index.js
+++ source/components/SocialLinks/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import Thumbnail from '../Thumbnail';
 import './assets/social-links.scss';
 
 const SocialLinks = props => {
-  const socialLink = props.data.map((l, index) =>
-    <li className="c-sm-links__item" key={index}><a href={l.href} /></li>);
+  const socialLink = props.data.map((l, index) =>(
+    <li className="c-sm-links__item" key={index}>
+      <a className="c-sm-links__link" href={l.href} target="_blank" rel="noopener">
+        <Thumbnail src={l.icon} />
+      </a>
+    </li>
+  ));
 
   return <ul className="c-sm-links">{socialLink}</ul>;
 };
