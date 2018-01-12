303680decc6c0d248c5292513c9eac884ccf6ce0
Filipe Fernandes <me@filipesf.com>
2018-01-12 23:52:37 +0000

    Added Console component

---
+++ source/containers/App/Console.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/console.scss';
+
+class Console extends React.Component {
+  render() {
+    return (
+      <section className="c-console" />
+    );
+  }
+}
+
+export default Console;
