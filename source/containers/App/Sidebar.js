74ba06913dc8179b095fb7f27b54d9a4eb54a834
Filipe Fernandes <me@filipesf.com>
2018-01-12 23:49:34 +0000

    Updated namespace

--- source/containers/App/Sidebar.js
+++ source/containers/App/Sidebar.js
@@ -6,29 +6,29 @@ import Thumbnail from '../../components/Thumbnail';
 import './assets/sidebar.scss';
 
 const Sidebar = () => (
-  <aside className="c-sidebar">
+  <aside className="l-sidebar">
     <Brand link="/" />
 
-    <nav className="c-sidebar__nav">
-      <Link to="/orcamento" className="c-sidebar__link">
+    <nav className="l-sidebar__nav">
+      <Link to="/orcamento" className="l-sidebar__link">
         <Thumbnail src={assets.icon.cursor} />
-        <span className="c-sidebar__text">Solicitar</span>
+        <span className="l-sidebar__text">Solicitar</span>
       </Link>
-      <Link to="/rastrear" className="c-sidebar__link">
+      <Link to="/rastrear" className="l-sidebar__link">
         <Thumbnail src={assets.icon.pin} />
-        <span className="c-sidebar__text">Rastrear</span>
+        <span className="l-sidebar__text">Rastrear</span>
       </Link>
-      <Link to="/ordens" className="c-sidebar__link">
+      <Link to="/ordens" className="l-sidebar__link">
         <Thumbnail src={assets.icon.orders} />
-        <span className="c-sidebar__text">Ordens</span>
+        <span className="l-sidebar__text">Ordens</span>
       </Link>
-      <Link to="/senha" className="c-sidebar__link">
+      <Link to="/senha" className="l-sidebar__link">
         <Thumbnail src={assets.icon.padlock} />
-        <span className="c-sidebar__text">Senha</span>
+        <span className="l-sidebar__text">Senha</span>
       </Link>
-      <Link to="/sair" className="c-sidebar__link">
+      <Link to="/sair" className="l-sidebar__link">
         <Thumbnail src={assets.icon.logout} />
-        <span className="c-sidebar__text">Sair</span>
+        <span className="l-sidebar__text">Sair</span>
       </Link>
     </nav>
   </aside>
