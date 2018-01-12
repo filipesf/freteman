4d6aba3c999253c540ec242ab735557746db94d2
Filipe Fernandes <me@filipesf.com>
2018-01-12 11:46:24 +0000

    Added estimate styles

--- source/containers/App/Estimates.js
+++ source/containers/App/Estimates.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Panel from '../../components/Panel';
+import './assets/estimates.scss';
 
 class Estimates extends React.Component {
   render() {
     return (
-      <section id="Estimates" />
+      <section className="l-estimates">
+        <Panel />
+        <Panel />
+      </section>
     );
   }
 }
