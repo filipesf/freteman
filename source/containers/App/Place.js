997ef84f6f667a4ffdd7f05ddeb063fb9b38b920
Filipe Fernandes <me@filipesf.com>
2018-01-14 22:57:22 +0000

    Added Select component; Moved dummy data to constants/options.js file

--- source/containers/App/Place.js
+++ source/containers/App/Place.js
@@ -2,17 +2,14 @@ import React from 'react';
 import propTypes from 'prop-types';
 import assets from '../../constants/assets';
 import { getPlaceIcon } from '../../constants/utils';
+import opts from '../../constants/options';
 import Thumbnail from "../../components/Thumbnail";
+import Select from "../../components/Form/Select";
 import InputText from "../../components/Form/InputText";
 import RadioGroup from "../../components/Form/RadioGroup";
 import './assets/place.scss';
 
 const Place = props => {
-  const hasLiftOptions = [
-    { id: 'Sim', label: 'Sim', value: 'Sim' },
-    { id: 'Não', label: 'Não', value: 'Não' }
-  ];
-
   return (
     <section className="l-place">
       <Thumbnail src={getPlaceIcon(props.point)} />
@@ -26,8 +23,8 @@ const Place = props => {
 
         <div className="l-place__details">
           <InputText id="falar-com" label="Falar com:" placeholder="Rodrigo Rodrigues" />
-          <InputText id="qual-andar" label="Qual Andar?" placeholder="12º" />
-          <RadioGroup flat label="Possui elevador?" name="elevador" options={hasLiftOptions} />
+          <Select id="qual-andar" label="Qual Andar?" name="andares" options={opts.floors} />
+          <RadioGroup flat label="Possui elevador?" name="elevador" options={opts.yN} />
         </div>
       </div>
     </section>
