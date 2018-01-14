a351954299e256bfdc82110e27337ad384dd2d3f
Filipe Fernandes <me@filipesf.com>
2018-01-14 22:54:03 +0000

    Added Select component

--- source/components/Form/Select.js
+++ source/components/Form/Select.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import assets from '../../constants/assets';
+import Thumbnail from '../Thumbnail';
+import Field from './Field';
+import Label from './Label';
 import './assets/select.scss';
 
 const Select = props => {
+  const selectOptions = props.options.map((s, i) => (
+    <option
+      key={i}
+      value={s.value}
+    >{s.label}</option>
+  ));
+
   return (
-    <select className="f-select">{props.options}</select>
+    <Field>
+      <Label text={props.label} />
+      <div className="f-select">
+        <Thumbnail src={assets.icon.arrowSimple} />
+        <select className="f-select__options" name={props.name}>
+          {selectOptions}
+        </select>
+      </div>
+    </Field>
   );
-}
+};
+
+Select.propTypes = {
+  id: propTypes.string.isRequired,
+  name: propTypes.string.isRequired,
+  label: propTypes.string,
+  options: propTypes.array.isRequired,
+};
 
 export default Select;
