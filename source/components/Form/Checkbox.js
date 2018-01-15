b9847e3c512c14cefbc413be5d1d5d4581c9538b
Filipe Fernandes <me@filipesf.com>
2018-01-15 22:45:31 +0000

    Added Checkbox markup and styles

--- source/components/Form/Checkbox.js
+++ source/components/Form/Checkbox.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { parseInputID } from '../../constants/utils';
+import assets from '../../constants/assets';
+import Field from './Field';
+import Thumbnail from '../Thumbnail';
 import './assets/checkbox.scss';
 
 const Checkbox = props => {
   return (
-    <div className="f-checkbox" />
+    <Field>
+      <input id={parseInputID(props.id)} type="checkbox" name={props.name} value={props.value} defaultChecked={props.checked} />
+      <label className="f-checkbox" htmlFor={parseInputID(props.id)}>
+        <Thumbnail src={assets.icon.check} />
+        <span className="f-checkbox__text">{props.label}</span>
+      </label>
+    </Field>
   );
-}
+};
+
+Checkbox.propTypes = {
+  id: propTypes.string.isRequired,
+  name: propTypes.string.isRequired,
+  value: propTypes.string.isRequired,
+  label: propTypes.string.isRequired,
+  checked: propTypes.bool,
+};
 
 export default Checkbox;
