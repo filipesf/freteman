a76c10262bb248b53be83d7fb3a424a9f699df71
Filipe Fernandes <me@filipesf.com>
2018-01-15 22:28:33 +0000

    Added helpers icons; added services options; Added ground floor

--- source/constants/options.js
+++ source/constants/options.js
@@ -1,4 +1,4 @@
-// import assets from './assets';
+import assets from './assets';
 
 const opts = {
   yN: [{
@@ -11,6 +11,7 @@ const opts = {
     value: 'Não'
   }],
   floors: [
+    { id: 'terreo', label: 'Térreo', value: 0 },
     { id: '1º', label: '1º', value: 1 },
     { id: '2º', label: '2º', value: 2 },
     { id: '3º', label: '3º', value: 3 },
@@ -35,22 +36,36 @@ const opts = {
     id: 'motorista-nao-ajuda',
     value: 'O motorista não carregará coisas',
     label: 'O motorista não carregará coisas',
-    // icon: assets.icon.helper0
+    icon: assets.icon.helpers0
   }, {
     id: 'motorista-ajuda',
     value: 'O motorista irá ajudar no transporte',
     label: 'O motorista irá ajudar no transporte',
-    // icon: assets.icon.helper1
+    icon: assets.icon.helpers1
   }, {
     id: 'motorista-1-ajudante',
     value: 'Motorista + 1 Ajudante',
     label: 'Motorista + 1 Ajudante',
-    // icon: assets.icon.helper2
+    icon: assets.icon.helpers2
   }, {
     id: 'motorista-2-ajudantes',
     value: 'Motorista + 2 Ajudantes',
     label: 'Motorista + 2 Ajudantes',
-    // icon: assets.icon.helper3
+    icon: assets.icon.helpers3
+  }],
+  services: [{
+    id: 'cargas-e-encomendas',
+    label: 'Cargas e encomendas',
+    value: 'Cargas e encomendas'
+  }],
+  payMethods: [{
+    id: 'pagamento-coleta',
+    label: 'R. Andaraí, Vila Mariana, 123 - São Paulo, SP',
+    value: 'R. Andaraí, Vila Mariana, 123 - São Paulo, SP'
+  }, {
+    id: 'pagamento-entrega',
+    label: 'R. Conselheiro Cotegipe, Belenzinho, 123 - São Paulo, SP',
+    value: 'R. Conselheiro Cotegipe, Belenzinho, 123 - São Paulo, SP'
   }]
 };
 
