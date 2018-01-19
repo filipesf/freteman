98a62fd1e7983d313d964067d324acabfe8aaaaf
Filipe Fernandes <me@filipesf.com>
2018-01-19 09:50:15 +0000

    Using fieldset instead place-actions styles

--- source/containers/App/PlaceActions.js
+++ source/containers/App/PlaceActions.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import Fieldset from '../../components/Form/Fieldset';
 import Checkbox from '../../components/Form/Checkbox';
 import '../../components/Button';
-import './assets/place-actions.scss';
 
 const PlaceActions = () => {
   return (
-    <section className="l-place-actions">
+    <Fieldset>
       <button className="c-btn --outline --primary">Adicionar Volta</button>
       <button className="c-btn --outline --primary">Adicionar Parada</button>
       <Checkbox id="recibo" name="recibo" label="Recibo digital obrigatório" value="1" checked />
-    </section>
+    </Fieldset>
   );
 };
 
