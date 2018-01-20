479710dd7091e0f4a5c0fadc2ae891d268c6b917
Filipe Fernandes <me@filipesf.com>
2018-01-20 17:15:12 +0000

    Added OrderSummary mockup data

---
+++ source/constants/summaryData.js
@@ -0,0 +1,27 @@
+const SummaryData = [{
+  name: "Distância",
+  value: "13km"
+}, {
+  name: "Detalhes do local",
+  value: "12º andar sem elevador"
+}, {
+  name: "Horário de Retirada",
+  value: "Dentro de 1h"
+}, {
+  name: "Veículo",
+  value: "Van"
+}, {
+  name: "Tipo de serviço",
+  value: "Cargas e encomendas"
+}, {
+  name: "Número de Ajudantes",
+  value: "Motorista + 2"
+}, {
+  name: "Local do pagamento",
+  value: "R. Andaraí, Vila Mariana, 123 - São Paulo, SP"
+}, {
+  name: "Método de pagamento",
+  value: "Cartão"
+}];
+
+export default SummaryData;
