d9e8b4131062a2c0c74b8d3f3490cd270b63bbc3
Filipe Fernandes <me@filipesf.com>
2018-01-20 12:07:00 +0000

    Made props.name optional

--- source/components/Form/Checkbox.js
+++ source/components/Form/Checkbox.js
@@ -9,7 +9,13 @@ import './assets/checkbox.scss';
 const Checkbox = props => {
   return (
     <Field>
-      <input id={parseInputID(props.id)} type="checkbox" name={props.name} value={props.value} defaultChecked={props.checked} />
+      <input
+        type="checkbox"
+        id={parseInputID(props.id)}
+        name={props.name ? props.name : props.id}
+        value={props.value}
+        defaultChecked={props.checked}
+      />
       <label className="f-checkbox" htmlFor={parseInputID(props.id)}>
         <Thumbnail src={assets.icon.check} />
         <span className="f-checkbox__text">{props.label}</span>
@@ -20,7 +26,7 @@ const Checkbox = props => {
 
 Checkbox.propTypes = {
   id: propTypes.string.isRequired,
-  name: propTypes.string.isRequired,
+  name: propTypes.string,
   value: propTypes.string.isRequired,
   label: propTypes.string.isRequired,
   checked: propTypes.bool,
