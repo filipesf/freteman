5d565ad84b8dedec872067f72db91e8b44266334
Filipe Fernandes <me@filipesf.com>
2018-01-22 22:42:38 +0000

    Added name attribute support

--- source/components/Form/InputText.js
+++ source/components/Form/InputText.js
@@ -2,20 +2,20 @@ import React from 'react';
 import propTypes from 'prop-types';
 import Field from './Field';
 import Label from './Label';
-import { parseInputID } from '../../constants/utils';
 import './assets/text.scss';
 
 const InputText = props => {
   return (
     <Field>
-      {props.label && <Label for={parseInputID(props.id)} text={props.label} />}
-      <input className="f-text" id={parseInputID(props.id)} type="text" placeholder={props.placeholder} />
+      {props.label && <Label for={props.id} text={props.label} />}
+      <input className="f-text" id={props.id} name={props.name ? props.name : props.id} type="text" placeholder={props.placeholder} />
     </Field>
   );
 };
 
 InputText.propTypes = {
   id: propTypes.string.isRequired,
+  name: propTypes.string,
   label: propTypes.string,
   placeholder: propTypes.string,
 };
