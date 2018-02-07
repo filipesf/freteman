f25b7196b112d52c2976eda1cb31841ac31104e1
Filipe Fernandes <me@filipesf.com>
2018-02-07 11:46:44 +0000

    Updated naming convention

--- source/constants/dataHome.js
+++ source/constants/dataHome.js
@@ -1,6 +1,6 @@
 import assets from './assets';
 
-const HomeData = {
+const dataHome = {
   header: {
     hero: assets.img.hero,
     featured: assets.img.featured2x,
@@ -95,64 +95,7 @@ const HomeData = {
       { image: assets.img.reclameaqui },
       { image: assets.img.consul },
     ],
-  },
-  footer: {
-    customerService: {
-      image: assets.img.man,
-      title: 'Nós estamos aqui para ajudar. Fale conosco.'
-    },
-    navs: [{
-      title : 'Empresa',
-      list: [{
-        text: 'Serviços',
-        href: '#Servicos'
-      }, {
-        text: 'Cidades Atendidas',
-        href: '#Cidades-Atendidas'
-      }, {
-        text: 'Seja um Freteman',
-        href: '#Seja-um-Freteman'
-      }]
-    }, {
-      title : 'Freteman',
-      list: [{
-        text: 'Seja um Freteman',
-        href: '#Seja-um-Freteman'
-      }]
-    }, {
-      title : 'Sobre',
-      list: [{
-        text: 'Trabalhe Conosco',
-        href: '#Trabalhe-Conosco'
-      }, {
-        text: 'Blog',
-        href: '#Blog'
-      }]
-    }, {
-      title : 'Dúvidas?',
-      list: [{
-        text: 'Perguntas Frequentes',
-        href: '#Perguntas-Frequentes'
-      }, {
-        text: 'Entre em contato',
-        href: '#Entre-em-contato'
-      }]
-    }],
-    contact: {
-      phone: '(11) 93944-1232',
-      email: 'contato@freteman.com.br',
-      socialLinks: [{
-        href: '#Facebook',
-        icon: assets.icon.fb,
-      }, {
-        href: '#Instagram',
-        icon: assets.icon.ig,
-      }, {
-        href: '#Linkedin',
-        icon: assets.icon.in,
-      }]
-    }
   }
 };
 
-export default HomeData;
+export default dataHome;
