d471f29dfb943bd43bbbe0000d739b5b7511eee4
Filipe Fernandes <me@filipesf.com>
2018-02-08 20:38:17 +0000

    Moved the general structure to layout container; redefined default page layout

--- source/containers/Layout/Page.js
+++ source/containers/Layout/Page.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import Navigation from '../../components/Navigation';
-import Footer from '../../components/Footer';
+import propTypes from 'prop-types';
+import Layout from './index';
+import Headline from '../../components/Headline';
+import './assets/page.scss';
 
 const Page = props => {
   return (
-    <section className="l-page">
-      <Navigation />
-      { props.children }
-      <Footer />
-    </section>
+    <Layout id={props.id}>
+      <section className="l-page">
+        {props.title && <div className="l-page__title">
+          <Headline title={props.title} subtitle={props.subtitle} left />
+        </div>}
+
+        <div className="l-page__container">
+          <div className={props.className}>
+            {props.children}
+          </div>
+        </div>
+      </section>
+    </Layout>
   );
+};
+
+Page.propTypes = {
+  id: propTypes.string,
+  className: propTypes.string,
+  title: propTypes.string,
+  subtitle: propTypes.string,
+  children: propTypes.node.isRequired
 };
 
 export default Page;
