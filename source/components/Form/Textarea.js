ad280b7a58d3efa577416b713972c099b23761af
Filipe Fernandes <me@filipesf.com>
2018-02-09 10:18:35 +0000

    Added support to name attribute that was missing

--- source/components/Form/Textarea.js
+++ source/components/Form/Textarea.js
@@ -2,20 +2,26 @@ import React from 'react';
 import propTypes from 'prop-types';
 import Field from './Field';
 import Label from './Label';
-import { parseInputID } from '../../constants/utils';
 import './assets/text.scss';
 
 const Textarea = props => {
   return (
     <Field>
-      {props.label && <Label for={parseInputID(props.id)} text={props.label} />}
-      <textarea className="f-textarea" id={parseInputID(props.id)} type="textarea" placeholder={props.placeholder} />
+      {props.label && <Label for={props.id} text={props.label} />}
+      <textarea
+        id={props.id}
+        className="f-textarea"
+        type="textarea"
+        name={props.name ? props.name : props.id}
+        placeholder={props.placeholder}
+      />
     </Field>
   );
 };
 
 Textarea.propTypes = {
   id: propTypes.string.isRequired,
+  name: propTypes.string,
   label: propTypes.string,
   placeholder: propTypes.string,
 };
