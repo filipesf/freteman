2398efa0fe5f3103c75ccbd0418436b938f7c5b7
Filipe Fernandes <me@filipesf.com>
2018-02-09 00:35:32 +0000

    Added Services and Contact pages

--- source/index.js
+++ source/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ServiceWorker from './sw';
 import App from './containers/App';
 import Home from './containers/Home';
+import Services from './containers/Services';
+import Contact from './containers/Contact';
 import Error404 from './containers/404';
 import './assets/styles/main.scss';
 
@@ -11,7 +13,9 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/orcamento" component={App} />
+      <Route path="/servicos" component={Services} />
+      <Route path="/contato" component={Contact} />
+      <Route path="/orcamento" component={App} />
       <Route component={Error404} />
     </Switch>
   </BrowserRouter>, document.getElementById('root'));
